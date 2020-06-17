ed579b5747ebc25f64230742fdf5198513a01435
febryardiansyah <febrymuhammad80@gmail.com>
2020-06-17 19:47:51 +0700

    initial

---
+++ detail_dialog.py
@@ -0,0 +1,54 @@
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(QtWidgets.QWidget):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(633, 446)
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(20, 10, 81, 16))
+        self.label.setObjectName("label")
+
+        self.listWidget = QtWidgets.QListWidget(Dialog)
+        self.listWidget.setGeometry(QtCore.QRect(20, 70, 591, 381))
+        self.listWidget.setObjectName("listWidget")
+
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(20, 40, 91, 16))
+        self.label_2.setObjectName("label_2")
+        
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(510, 10, 113, 32))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+        self.pushButton.clicked.connect(self.closeFun)
+    
+    def closeFun(self):
+        print('close')
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog",''))
+        self.label_2.setText(_translate("Dialog", "Arti Indonesia"))
+        self.pushButton.setText(_translate("Dialog", "Close"))
+    def getSurat(self,data):
+        suratList = data['ayahs']
+        self.label.setText(suratList['englishName'])
+        # for item in suratList:
+        #     self.listWidget.addItem(item['text']['arab'])
+        #     print(item['text']['arab'])
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
---
+++ mainUi.py
@@ -0,0 +1,89 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from detail_dialog import *
+import requests
+
+baseUrl = 'https://api.quran.sutanlab.id/surah/'
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(625, 403)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+
+        self.surat = QtWidgets.QLabel('Surat-surat Al-\'quran')
+        self.daftarSurat = QtWidgets.QListWidget()
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+
+        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
+        self.scrollArea.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents_2 = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents_2.setGeometry(QtCore.QRect(0, 0, 584, 332))
+        self.scrollAreaWidgetContents_2.setObjectName("scrollAreaWidgetContents_2")
+
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents_2)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.verticalLayout_2.addWidget(self.daftarSurat)
+
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents_2,)
+        self.verticalLayout.addWidget(self.surat)
+        self.verticalLayout.addWidget(self.scrollArea)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        self.daftarSurat.doubleClicked.connect(self.suratDetail)
+        
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Al-Qur\'an"))
+        
+    def fetchSurat(self):
+        self.getAllSurat = requests.get(baseUrl).json()
+        data = self.getAllSurat['data']
+        self.listSurat(data)
+
+    def listSurat(self,data=[]):
+        for item in data:
+            self.daftarSurat.addItem(item['englishName'])
+    
+    def suratDetail(self):
+        Dialog = QtWidgets.QDialog()
+        ui = Ui_Dialog()
+        self.getDetailSurat = requests.get(baseUrl+str(self.daftarSurat.currentRow()+1)).json()
+        data = self.getDetailSurat['data']
+        ui.getSurat(data)
+        ui.setupUi(Dialog)     
+        Dialog.show()
+        Dialog.exec_()
+
+
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    ui.fetchSurat()
+    MainWindow.show()
+    sys.exit(app.exec_())
